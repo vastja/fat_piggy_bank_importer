c215ca295e504ca925dac98d15021c0e81c00315
vastja <vastja@gmail.com>
2025-03-29 21:37:53 +0100

    Save expenses to database

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,42 @@
 use rusqlite::Connection;
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    result::Result,
-};
 
 fn main() {
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read command");
+    let expenses = vec![Expense {
+        date: String::from("17-5-2025"),
+        tag: String::from("Present"),
+        amount: 100,
+    }];
 
-    let command: Vec<&str> = input.trim().split(' ').collect();
+    let connection = Connection::open("./fat_piggy_bank.db").expect("Database connection failed.");
 
-    match command[0] {
-        "init" => {
-            let config = Config {
-                location: command[1].to_string(),
-            };
-            init_db(&config);
-            print!("Database initialized.");
-        }
-        _ => println!("{} command not recognized.", input),
+    connection
+        .execute(
+            "CREATE TABLE expenses (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date DATE,
+                    tag TEXT NOT NULL,
+                    amount INTEGER
+                )",
+            (), // empty list of parameters.
+        )
+        .expect("Table creation failed.");
+
+    for expense in expenses {
+        connection
+            .execute(
+                "INSERT INTO expenses (date, tag, amount) VALUES (?1, ?2, ?3)",
+                [
+                    expense.date.clone(),
+                    expense.tag.clone(),
+                    expense.amount.to_string(),
+                ],
+            )
+            .expect("Table creation failed.");
     }
 }
 
-struct Config {
-    location: String,
-}
-
-fn init_db(config: &Config) {
-    let conn = Connection::open(&config.location).expect("Database connection failed.");
-
-    conn.execute(
-        "CREATE TABLE expenses (
-            id INTEGER PRIMARY KEY,
-            date DATE
-            type TEXT NOT NULL,
-            amount INTEGER
-        )",
-        (), // empty list of parameters.
-    )
-    .expect("Table creation failed.");
-}
-
-struct Expense {}
-
-fn load_reports(path: &Path) -> std::io::Result<Vec<Expense>> {
-    let reports: Vec<PathBuf> = match path.is_dir() {
-        true => path
-            .read_dir()?
-            .map(|entry| entry.map(|x| x.path()))
-            .collect::<Result<Vec<_>, _>>()?,
-        false => vec![PathBuf::from(path)],
-    };
-
-    let expenses = reports
-        .into_iter()
-        .map(|x| load_report(x.as_path()))
-        .collect::<std::io::Result<Vec<_>>>()?
-        .into_iter()
-        .flatten()
-        .collect();
-
-    Ok(expenses)
-}
-
-fn load_report(path: &Path) -> std::io::Result<Vec<Expense>> {
-    let expenses: Vec<Expense> = fs::read_to_string(path)?
-        .lines()
-        .map(|_| Expense {})
-        .collect();
-
-    Ok(expenses)
+struct Expense {
+    date: String,
+    tag: String,
+    amount: i32,
 }
